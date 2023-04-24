d69bf82c3817f449902ff48f22bd5484b893ee90
Daniel Rosel <daniel@alves.world>
2023-04-24 15:46:19 +0200

    fresh

--- student.py
+++ student.py
@@ -2,6 +2,8 @@ from langchain.agents import load_tools, Tool
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
 
 llm = OpenAI(temperature=0.7)
 
@@ -9,19 +11,35 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Pinecone
 from langchain.document_loaders import TextLoader
-#loader = TextLoader('/mnt/s/Documents/Collage/university-study-notes/content/20221231174507-fundamentals_of_data_analysis_class_notes.org')
 from langchain.document_loaders import UnstructuredPDFLoader
-loader = UnstructuredPDFLoader('/home/velocitatem/Downloads/Hypothesis Testing One Sample-3.pdf')
-documents = loader.load()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+
+base_loader = TextLoader('base.txt')
+base_documents = base_loader.load()
+default_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = default_splitter.split_documents(base_documents)
+
+
+import json
+# load config.json
+with open('config.json') as f:
+    config = json.load(f)
+
+for source in config['sources']:
+    # source is a file name
+    suffix = source.split('.')[-1]
+    if suffix == 'txt' or suffix == 'org':
+        loader = TextLoader(source)
+    elif suffix == 'pdf':
+        loader = UnstructuredPDFLoader(source)
+
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts.extend(text_splitter.split_documents(documents))
 
 embeddings = OpenAIEmbeddings()
-from langchain.vectorstores import Chroma
+
 docsearch = Chroma.from_documents(texts, embeddings)
 
-from langchain.llms import OpenAI
-from langchain.chains import RetrievalQA
 qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=docsearch.as_retriever())
 
 stats_tool = Tool(
@@ -29,13 +47,17 @@ stats_tool = Tool(
     description="Search notes on statistics and how to use python to calculate them. You can also ask what to do when solving some problem.",
     func=qa.run
 )
+
+def search_notes(query):
+    return qa.run(query)
+
 from langchain.tools import DuckDuckGoSearchTool
 ddg = DuckDuckGoSearchTool()
 from langchain.utilities import PythonREPL
 repl = PythonREPL()
 tools = []
 tools.append(stats_tool)
-tools.append(ddg)
+#tools.append(ddg)
 python_calc_tool = Tool(
     name="python",
     description="Run python code in the REPL. Or execute some calculation. Do not search any information here.",
@@ -43,52 +65,42 @@ python_calc_tool = Tool(
 )
 tools.append(python_calc_tool)
 
-from langchain.memory import ConversationBufferMemory
-memory = ConversationBufferMemory(memory_key="chat_history")
 
-agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, memory=memory)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, return_intermediate_steps=True)
 
 template = '''
-:State the null and alternative hypotheses;
-:Identify the test statistic;
-if (Population standard deviation is known?) then (yes)
-  if (Sample size is large?) then (yes)
-    :Use the z-test;
-  else (no)
-    :Use the t-test;
-  endif
-else (no)
-  :Use the t-test;
-endif
-:Find the p-value;
-if (p <= alpha) then (yes)
-  :Reject H0;
-else (no)
-  :Fail to reject H0;
-endif
-if (p <= alpha) then (yes)
-  :The result is statistically significant;
-else (no)
-  :The result is not statistically significant;
-endif
-
-You will not always have to use the above methodology, only use it if you should conduct an entire hypothesis test. Solve the following problem:
+Context:
 {problem}
-We also need to know the following intermediate steps:
+---
+Given the above problem, solve it and select one of the following options. Return only the number of the correct option.
 {need_to_know}
 ---
-When using the Python tool, always print all the variables you want to see. For example, if you want to see the value of x, you should print(x) instead of just x.
+When using the Python tool, always print all the variables you want to see. For example, if you want to see the value of x, you should print(x) instead of just x. You must only respond with the number of the correct option, no other text is allowed.
 '''
 
-while True:
-    task = input("Task: ")
-    if task == "exit":
-        break
-    if task == 'solve':
-        prompt = input("Context of Question: ")
-        prompt_2 = input("Goals: ")
-        response = agent.run(template.format(problem=prompt, need_to_know=prompt_2))
-        print("A: ", response)
-    else:
-        response = agent.run(task)
-        print("A: ", response)
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+import json
+
+@app.route('/api/v1/ask', methods=['POST'])
+def api_id():
+    # implement the solve task
+    context = request.json['context']
+    goals = request.json['goals']
+    inall = template.format(problem=context, need_to_know=goals)
+    response = agent({inall})
+    return jsonify({'response': response['output'], 'intermediate_steps': response['intermediate_steps']})
+
+# add route for the search notes method
+@app.route('/api/v1/search', methods=['POST'])
+def api_search():
+    query = request.json['query']
+    response = search_notes(query)
+    return jsonify({'response': response})
+
+
+
+app.run()
---
+++ student_gui.py
@@ -0,0 +1,75 @@
+# @app.route('/api/v1/ask', methods=['post'])
+# def api_id():
+#     # implement the solve task
+#     context = request.json['context']
+#     goals = request.json['goals']
+
+#     response = agent.run(template.format(problem=context, need_to_know=goals))
+#     return personify(response)
+import requests
+def get_answer(context, goals):
+    url = 'http://localhost:5000/api/v1/ask'
+    myobj = {'context': context, 'goals': goals}
+    x = requests.post(url, json = myobj)
+    x_json = x.json()
+    return (x_json['response'], x_json['intermediate_steps'])
+
+
+# @app.route('/api/v1/search', methods=['post'])
+# def api_search():
+#     query = request.json['query']
+#     response = search_notes(query)
+#     return jsonify({'response': response})
+def search_notes(query):
+    url = 'http://localhost:5000/api/v1/search'
+    myobj = {'query': query}
+    x = requests.post(url, json = myobj)
+    x_json = x.json()
+    return x_json['response']
+
+import streamlit as st
+
+st.title("StraightAGPT")
+
+
+st.header("Search for notes")
+# get search query from user
+query = st.text_input("Search query")
+# button to submit the search query
+submit_button = st.button("Search")
+# if the button is pressed
+if submit_button:
+    # show a loading indicator
+    with st.spinner("Searching..."):
+        response = search_notes(query)
+    # display the answer streamed from the model
+    st.write(response)
+
+
+st.header("Solve Problems")
+# get context input from user (large text box)
+context = st.text_area("Context", height=200)
+
+# get question input from user (single line text box)
+question = st.text_input("Question")
+
+# button to submit the question
+submit_button = st.button("Answer")
+
+# if the button is pressed
+if submit_button:
+    # show a loading indicator
+    with st.spinner("Thinking..."):
+        response = get_answer(context, question)
+        steps = response[1]
+        response = response[0]
+    # display the answer streamed from the model
+    st.write(response)
+    for step in steps:
+        st.write(step)
+    # show a button to add this response to the context
+    add_to_context = st.button("Add to context")
+    if add_to_context:
+        context_new = f"{question}: {response}"
+        # update the context text area with the new context
+        context = st.text_area("Context", context_new, height=200)
